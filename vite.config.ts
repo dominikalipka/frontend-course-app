e3b815f9cc5c0b008787d7b8f77d0668142a2d23
Dominika Lipka <dominika.lipka96@gmail.com>
2024-05-09 14:51:27 +0200

    Add vite-plugin-image-optimizer to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    sourcemap: true,
+  },
+
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 50,
+      },
+    }),
+  ],
 });
